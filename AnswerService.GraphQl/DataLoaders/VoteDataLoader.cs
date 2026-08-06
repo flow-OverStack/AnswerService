b1468fb3fc3288fce4403722740a9a9ed7d6b349
dotneteeer <447599489marat@gmail.com>
2026-08-06 18:45:40 +0300

    refactor: unify DataLoaders on generic base

--- AnswerService.GraphQl/DataLoaders/VoteDataLoader.cs
+++ AnswerService.GraphQl/DataLoaders/VoteDataLoader.cs
@@ -1,6 +1,8 @@
 using AnswerService.Application.Queries.Vote;
 using AnswerService.Domain.Dtos.Vote;
 using AnswerService.Domain.Entities;
+using AnswerService.Domain.Results;
+using AnswerService.GraphQl.DataLoaders.Base;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,24 +12,11 @@ public class VoteDataLoader(
     IBatchScheduler batchScheduler,
     DataLoaderOptions options,
     IServiceScopeFactory scopeFactory)
-    : BatchDataLoader<VoteDto, Vote>(batchScheduler, options)
+    : EntityBatchDataLoader<Vote, VoteDto>(batchScheduler, options, scopeFactory)
 {
-    protected override async Task<IReadOnlyDictionary<VoteDto, Vote>> LoadBatchAsync(IReadOnlyList<VoteDto> keys,
-        CancellationToken cancellationToken)
-    {
-        await using var scope = scopeFactory.CreateAsyncScope();
-        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-        var query = new GetVotesQuery(keys);
+    protected override Task<CollectionResult<Vote>> FetchAsync(IServiceProvider scopedProvider,
+        IReadOnlyList<VoteDto> keys, CancellationToken cancellationToken) =>
+        scopedProvider.GetRequiredService<IMediator>().Send(new GetVotesQuery(keys), cancellationToken);
 
-        var result = await mediator.Send(query, cancellationToken);
-
-        var dictionary = new Dictionary<VoteDto, Vote>();
-
-        if (!result.IsSuccess)
-            return dictionary.AsReadOnly();
-
-        dictionary = result.Data.ToDictionary(x => new VoteDto(x.AnswerId, x.UserId), x => x);
-
-        return dictionary.AsReadOnly();
-    }
+    protected override VoteDto GetId(Vote entity) => new(entity.AnswerId, entity.UserId);
 }
